e3276a2c3b2340e8be25b16cc5ae11b553e7d7c5
Hilton Wesley <wesleysousalacerda@gmail.com>
2015-02-23 14:33:55 -0500

    Menu_inflated

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,32 @@ public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
     }
+
+
+    @Override
+        public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                // Add this line in order for this fragment to handle menu events.
+                        setHasOptionsMenu(true);
+            }
+
+                @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+                inflater.inflate(R.menu.forecastfragment, menu);
+            }
+
+                @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                        // automatically handle clicks on the Home/Up button, so long
+                                // as you specify a parent activity in AndroidManifest.xml.
+                                        int id = item.getItemId();
+                if (id == R.id.action_refresh) {
+                        return true;
+                    }
+                return super.onOptionsItemSelected(item);
+            }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
