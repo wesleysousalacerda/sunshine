217060ca1591c2d90ca5a646de14964ba8343a13
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-02-26 02:33:47 -0500

    LOG TAG String

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
@@ -158,9 +158,10 @@ public class ForecastFragment extends Fragment {
                         return null;
                     }
                     forecastJsonStr = buffer.toString();
+                    Log.v(LOG_TAG,"Forecast JSON String: "+ forecastJsonStr);
                 } catch (IOException e) {
                     Log.e(LOG_TAG, "Error ", e);
-                    // If the code didn't successfully get the weather data, there's no point in attemping
+                    // If the code didn't successfully get the weather data, there's no point in attempting
                     // to parse it.
                     return null;
                 } finally {
