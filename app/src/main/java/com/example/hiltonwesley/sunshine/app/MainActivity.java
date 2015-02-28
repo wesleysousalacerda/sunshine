4ef36611b1a1b5cf24fb101143f0103285be07d7
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-02-28 02:56:13 -0500

    Add MAP Location

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hiltonwesley.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,7 +14,7 @@ import com.example.android.sunshine.app.R;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +34,7 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -42,8 +47,34 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPrefs =
+                                PreferenceManager.getDefaultSharedPreferences(this);
+                String location = sharedPrefs.getString(
+                                getString(R.string.pref_location_key),
+                                getString(R.string.pref_location_default));
+
+                        // Using the URI scheme for showing a location found on a map.  This super-handy
+                         // intent can is detailed in the "Common Intents" page of Android's developer site:
+                                 // http://developer.android.com/guide/components/intents-common.html#Maps
+                                 Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                                .appendQueryParameter("q", location)
+                                .build();
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+
+                         if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+                    }
+            }
 
 }
