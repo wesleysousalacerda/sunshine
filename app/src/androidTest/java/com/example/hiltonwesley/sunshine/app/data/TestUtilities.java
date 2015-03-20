12aadfc6479d688e99c310ac90fbcdb0bfd30989
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-03-20 02:52:18 -0400

    Replaced WeatherContract and DBHelper - Corrected package

--- app/src/androidTest/java/com/example/hiltonwesley/sunshine/app/data/TestUtilities.java
+++ app/src/androidTest/java/com/example/hiltonwesley/sunshine/app/data/TestUtilities.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.test.AndroidTestCase;
 
-import com.example.android.sunshine.app.utils.PollingCheck;
+import com.example.hiltonwesley.sunshine.app.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
--- app/src/androidTest/java/com/example/hiltonwesley/sunshine/app/utils/PollingCheck.java
+++ app/src/androidTest/java/com/example/hiltonwesley/sunshine/app/utils/PollingCheck.java
@@ -15,7 +15,7 @@
  *
  * Note: This file copied from the Android CTS Tests
  */
-package com.example.android.sunshine.app.utils;
+package com.example.hiltonwesley.sunshine.app.utils;
 
 import junit.framework.Assert;
 
