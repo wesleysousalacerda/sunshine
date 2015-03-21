bec9bd352a8da9978a68100e56bd4ff09800a131
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-03-21 18:53:06 -0400

    Fixed Resource file

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/data/WeatherContract.java
@@ -30,15 +30,15 @@ public class WeatherContract {
     // relationship between a domain name and its website.  A convenient string to use for the
     // content authority is the package name for the app, which is guaranteed to be unique on the
     // device.
-    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+    public static final String CONTENT_AUTHORITY = "com.example.hiltonwesley.sunshine.app";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
-    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
+    // For instance, content://com.example.hiltonwesley.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.hiltonwesley.sunshine.app/givemeroot/ will fail,
     // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
     // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
     public static final String PATH_WEATHER = "weather";
