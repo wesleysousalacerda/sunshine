0045f8de1c92d6431a7e8cda7457f09d8185dd0c
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-03-22 02:44:03 -0400

    ContentValue at BulkInsert  Added

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/FetchWeatherTask.java
@@ -302,7 +302,9 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
             // add to database
             if ( cVVector.size() > 0 ) {
-                // Student: call bulkInsert to add the weatherEntries to the database here
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                                cVVector.toArray(cvArray);
+                                mContext.getContentResolver().bulkInsert(WeatherEntry.CONTENT_URI, cvArray);
             }
 
             // Sort order:  Ascending, by date.
