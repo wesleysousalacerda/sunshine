17991f78c8d84d26d2298c9c1e676eecd1bce66e
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-03-29 20:30:35 -0400

    SyncAdapter

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/ForecastFragment.java
@@ -1,5 +1,5 @@
 package com.example.hiltonwesley.sunshine.app;
-import android.content.Intent;
+
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,11 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.hiltonwesley.sunshine.app.data.WeatherContract;
-import com.example.hiltonwesley.sunshine.app.service.SunshineService;
-
-/**
- * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
- */
+import com.example.hiltonwesley.sunshine.app.sync.SunshineSyncAdapter;
 public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private ForecastAdapter mForecastAdapter;
 
@@ -165,10 +161,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        Intent intent = new Intent(getActivity(), SunshineService.class);
-        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
-                Utility.getPreferredLocation(getActivity()));
-        getActivity().startService(intent);
+        //String location = Utility.getPreferredLocation(getActivity());
+                //new FetchWeatherTask(getActivity()).execute(location);
+                        SunshineSyncAdapter.syncImmediately(getActivity());
     }
 
     @Override
---
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/sync/SunshineSyncService.java
@@ -0,0 +1,28 @@
+package com.example.hiltonwesley.sunshine.app.sync;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.app.Service;
+/**
+ * Created by HiltonWesley on 3/29/2015.
+ */
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
