f0beaa9402eee056d9f519081523f74f6a8a6e6f
wesleysousalacerda <wesleysousalacerda@gmail.com>
2015-05-28 01:41:24 -0400

    Revert "delete_old_data"
    This reverts commit bc8ab5f97e592ee599ab7b1c1fd1fff425c6185d.

--- app/src/main/java/com/example/hiltonwesley/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/hiltonwesley/sunshine/app/sync/SunshineSyncAdapter.java
@@ -1,4 +1,5 @@
 package com.example.hiltonwesley.sunshine.app.sync;
+
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.NotificationManager;
@@ -293,11 +294,6 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 ContentValues[] cvArray = new ContentValues[cVVector.size()];
                 cVVector.toArray(cvArray);
                 getContext().getContentResolver().bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, cvArray);
-
-                // delete old data so we don't build up an endless history
-                getContext().getContentResolver().delete(WeatherContract.WeatherEntry.CONTENT_URI,
-                        WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
-                        new String[] {Long.toString(dayTime.setJulianDay(julianStartDay-1))});
 
                 notifyWeather();
             }
